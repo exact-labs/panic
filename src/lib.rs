f21e5e5d1d0c6490f11cd09691867b40b059d82d
theMackabu <theMackabu@gmail.com>
2023-02-09 10:55:48 -0800

    change test style

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,3 @@
-//! Panic messages for humans
-//!
-//! Handles panics by calling
-//! [`std::panic::set_hook`](https://doc.rust-lang.org/std/panic/fn.set_hook.html)
-//! to make errors nice for humans.
-//!
-//! ## Why?
-//! When you're building a CLI, polish is super important. Even though Rust is
-//! pretty great at safety, it's not unheard of to access the wrong index in a
-//! vector or have an assert fail somewhere.
-//!
-//! When an error eventually occurs, you probably will want to know about it. So
-//! instead of just providing an error message on the command line, we can create a
-//! call to action for people to submit a report.
-//!
-//! This should empower people to engage in communication, lowering the chances
-//! people might get frustrated. And making it easier to figure out what might be
-//! causing bugs.
-//!
-//! ### Default Output
-//!
-//! ```txt
-//! thread 'main' panicked at 'oops', examples/main.rs:2:3
-//! note: Run with `RUST_BACKTRACE=1` for a backtrace.
-//! ```
-//!
-//! ### Human-Panic Output
-//!
-//! ```txt
-//! Well, this is embarrassing.
-//!
-//! human-panic had a problem and crashed. To help us diagnose the problem you can send us a crash report.
-//!
-//! We have generated a report file at "/var/folders/zw/bpfvmq390lv2c6gn_6byyv0w0000gn/T/report-8351cad6-d2b5-4fe8-accd-1fcbf4538792.toml". Submit an issue or email with the subject of "human-panic Crash Report" and include the report as an attachment.
-//!
-//! - Homepage: https://github.com/rust-ci/human-panic
-//! - Authors: Yoshua Wuyts <yoshuawuyts@gmail.com>
-//!
-//! We take privacy seriously, and do not perform any automated error collection. In order to improve the software, we rely on people to submit reports.
-//!
-//! Thank you kindly!
-
 #![cfg_attr(feature = "nightly", deny(missing_docs))]
 #![cfg_attr(feature = "nightly", feature(panic_info_message))]
 
@@ -51,21 +9,13 @@ use std::io::Result as IoResult;
 use std::panic::PanicInfo;
 use std::path::{Path, PathBuf};
 
-/// A convenient metadata struct that describes a crate
-///
-/// See [`metadata!`]
 pub struct Metadata {
-    /// The crate version
     pub version: Cow<'static, str>,
-    /// The crate name
     pub name: Cow<'static, str>,
-    /// The list of authors of the crate
     pub authors: Cow<'static, str>,
-    /// The URL of the crate's website
     pub homepage: Cow<'static, str>,
 }
 
-/// Initialize [`Metadata`]
 #[macro_export]
 macro_rules! metadata {
     () => {
@@ -78,25 +28,6 @@ macro_rules! metadata {
     };
 }
 
-/// `human-panic` initialisation macro
-///
-/// You can either call this macro with no arguments `setup_panic!()` or
-/// with a Metadata struct, if you don't want the error message to display
-/// the values used in your `Cargo.toml` file.
-///
-/// The Metadata struct can't implement `Default` because of orphan rules, which
-/// means you need to provide all fields for initialisation.
-///
-/// ```
-/// use human_panic::setup_panic;
-///
-/// setup_panic!(Metadata {
-///     name: env!("CARGO_PKG_NAME").into(),
-///     version: env!("CARGO_PKG_VERSION").into(),
-///     authors: "My Company Support <support@mycompany.com>".into(),
-///     homepage: "support.mycompany.com".into(),
-/// });
-/// ```
 #[macro_export]
 macro_rules! setup_panic {
     ($meta:expr) => {
@@ -112,8 +43,7 @@ macro_rules! setup_panic {
 
                 panic::set_hook(Box::new(move |info: &PanicInfo| {
                     let file_path = handle_dump(&meta, info);
-                    print_msg(file_path, &meta)
-                        .expect("human-panic: printing error message to console failed");
+                    print_msg(file_path, &meta).expect("panic: printing error message to console failed");
                 }));
             }
         }
@@ -124,12 +54,9 @@ macro_rules! setup_panic {
     };
 }
 
-/// Style of panic to be used
 #[derive(Copy, Clone, PartialEq, Eq)]
 pub enum PanicStyle {
-    /// Normal panic
     Debug,
-    /// Human-formatted panic
     Human,
 }
 
@@ -146,24 +73,17 @@ impl Default for PanicStyle {
     }
 }
 
-/// Utility function that prints a message to our human users
 #[cfg(feature = "color")]
 pub fn print_msg<P: AsRef<Path>>(file_path: Option<P>, meta: &Metadata) -> IoResult<()> {
     use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, WriteColor};
 
     let stderr_support = concolor::get(concolor::Stream::Stdout);
-    let choice = if stderr_support.color() {
-        ColorChoice::Always
-    } else {
-        ColorChoice::Never
-    };
-
+    let choice = if stderr_support.color() { ColorChoice::Always } else { ColorChoice::Never };
     let stderr = BufferWriter::stderr(choice);
     let mut buffer = stderr.buffer();
+
     buffer.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
-
     write_msg(&mut buffer, file_path, meta)?;
-
     buffer.reset()?;
 
     stderr.print(&buffer).unwrap();
@@ -175,17 +95,11 @@ pub fn print_msg<P: AsRef<Path>>(file_path: Option<P>, meta: &Metadata) -> IoRes
     let mut buffer = std::io::stderr();
 
     write_msg(&mut buffer, file_path, meta)?;
-
     Ok(())
 }
 
-fn write_msg<P: AsRef<Path>>(
-    buffer: &mut impl std::io::Write,
-    file_path: Option<P>,
-    meta: &Metadata,
-) -> IoResult<()> {
-    let (_version, name, authors, homepage) =
-        (&meta.version, &meta.name, &meta.authors, &meta.homepage);
+fn write_msg<P: AsRef<Path>>(buffer: &mut impl std::io::Write, file_path: Option<P>, meta: &Metadata) -> IoResult<()> {
+    let (_version, name, authors, homepage) = (&meta.version, &meta.name, &meta.authors, &meta.homepage);
 
     writeln!(buffer, "Well, this is embarrassing.\n")?;
     writeln!(
@@ -222,7 +136,6 @@ fn write_msg<P: AsRef<Path>>(
     Ok(())
 }
 
-/// Utility function which will handle dumping information to disk
 pub fn handle_dump(meta: &Metadata, panic_info: &PanicInfo) -> Option<PathBuf> {
     let mut expl = String::new();
 
@@ -230,10 +143,7 @@ pub fn handle_dump(meta: &Metadata, panic_info: &PanicInfo) -> Option<PathBuf> {
     let message = panic_info.message().map(|m| format!("{}", m));
 
     #[cfg(not(feature = "nightly"))]
-    let message = match (
-        panic_info.payload().downcast_ref::<&str>(),
-        panic_info.payload().downcast_ref::<String>(),
-    ) {
+    let message = match (panic_info.payload().downcast_ref::<&str>(), panic_info.payload().downcast_ref::<String>()) {
         (Some(s), _) => Some(s.to_string()),
         (_, Some(s)) => Some(s.to_string()),
         (None, None) => None,
@@ -245,11 +155,7 @@ pub fn handle_dump(meta: &Metadata, panic_info: &PanicInfo) -> Option<PathBuf> {
     };
 
     match panic_info.location() {
-        Some(location) => expl.push_str(&format!(
-            "Panic occurred in file '{}' at line {}\n",
-            location.file(),
-            location.line()
-        )),
+        Some(location) => expl.push_str(&format!("Panic occurred in file '{}' at line {}\n", location.file(), location.line())),
         None => expl.push_str("Panic location unknown.\n"),
     }
 
--- test/src/main.rs
+++ test/src/main.rs
@@ -1,7 +1,5 @@
-use human_panic::setup_panic;
-
 fn main() {
-    setup_panic!(Metadata {
+    exact_panic::setup_panic!(Metadata {
         name: env!("CARGO_PKG_NAME").into(),
         version: env!("CARGO_PKG_VERSION").into(),
         authors: "My Company Support <support@mycompany.com".into(),
