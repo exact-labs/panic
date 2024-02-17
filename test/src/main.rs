c2e0430a7649dbc1a1e4c8eaea1ff356cceb8dc4
theMackabu <theMackabu@gmail.com>
2024-02-17 13:52:32 -0800

    update macros

--- test/src/main.rs
+++ test/src/main.rs
@@ -1,4 +1,4 @@
-use panic::{create_messages, setup_panic};
+use panic::setup_panic;
 
 fn main() {
     setup_panic! {
@@ -6,10 +6,11 @@ fn main() {
         short_name: "panic",
         version: env!("CARGO_PKG_VERSION"),
         repository: "https://github.com/exact-labs/panic",
-        message: create_messages! {
-            head: "Well, this is embarrassing.\n%(name) v%(version) had a problem and crashed. To help us diagnose the problem you can send us a crash report\n",
-            body: "We have generated a report file at \"%(file_path)\". Submit an issue or email with the subject of \"%(name) v%(version) crash report\" and include the report as an attachment at %(repository).\n",
-            footer: "We take privacy seriously, and do not perform any automated error collection. In order to improve the software, we rely on people to submit reports. \nThank you!"
+        messages: {
+            colors: (Color::Red, Color::White, Color::Green),
+            head: "Well, this is embarrassing. %(name) v%(version) had a problem and crashed. \nTo help us diagnose the problem you can send us a crash report\n",
+            body: "We have generated a report file at \"%(file_path)\". \nSubmit an issue or email with the subject of \"%(name) v%(version) crash report\" and include the report as an attachment at %(repository).\n",
+            footer: "We take privacy seriously, and do not perform any automated error collection. \nIn order to improve the software, we rely on people to submit reports. Thank you!"
         }
     };
 
